b9b1604d27e30bd01090ce9597ccb02d02d07994
高松大樹 <suguer8x3@yahoo.co.jp>
2017-05-03 15:15:02 +0900

    ScriptTestの作成と課題提出
    2度目のコミットです上手くいってますか？

---
+++ Assets/Boss.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss {
+    private int hp = 100;　　　//体力
+    private int power = 25;  //攻撃力
+    private int mp = 53;       //MPを初期化
+
+    //攻撃用の関数
+    public void Attack(){
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    //防御用の関数
+    public void Defence(int damage){
+        Debug.Log(damage + "のダメージを受けた");
+        //残りhpを減らす
+        this.hp -= damage;
+       
+    }
+    
+//発展課題　　魔法攻撃をするMagic関数
+    public void Magic(int shouhi){
+        this.mp -= shouhi;
+        if (mp > shouhi){
+
+            Debug.Log("魔法攻撃をした。残りＭＰは" + mp + "です。");
+        }else{
+            Debug.Log("ＭＰが足りない！！");
+        }
+       
+    }
+
+    
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //課題　要素数5の配列を初期化する
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < array.Length; i++)
+        {
+            //課題　各要素の値を順番に表示
+            Debug.Log(array[i]);
+        }
+        //課題 各要素の値を逆順に表示
+        Debug.Log("課 題 2");
+        for (int i = array.Length - 1; 0 <= i; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+//発展課題　　魔法攻撃用の関数を呼び出す
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        lastboss.Magic(5);
+        
+    }
+   
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
